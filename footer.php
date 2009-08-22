8ecb83cb02e39799c8ed6a31e0d4892bd702f58f
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-08-22 15:59:18 -0700

    Removed SpamPoison
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- footer.php
+++ footer.php
@@ -12,7 +12,6 @@
 		<p>
 			&copy; <?php echo date('Y'); ?> <?php echo the_author_meta('firstname',0); echo "&nbsp;"; echo the_author_meta('lastname'); ?>. <?php bloginfo('name') ?> is powered by <a href="http://wordpress.org/">WordPress</a> with <a href="http://comicpress.org/">ComicPress</a>
 			| Subscribe: <a href="<?php bloginfo('rss2_url') ?>">RSS Feed</a> | <a href="#outside" onclick="scrollup(); return false;">Return to Top &nbsp;</a><br />
-			<a href="http://english-156752579142.spampoison.com"><img src="http://pics4.inxhost.com/images/sticker.gif" alt="Spam Poison" border="0" width="80" height="15"/></a><br />
 			<!-- <?php echo get_num_queries() ?> queries. <?php timer_stop(1) ?> seconds. -->
 		</p>
 </div>
