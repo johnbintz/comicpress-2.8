3ad8f2703679f987fc6d10f69384193503bc0ab5
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-08-28 02:26:39 -0700

    Check if the page is paginated so not to display the pagination bar if not.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- functions/pagination.php
+++ functions/pagination.php
@@ -2,16 +2,18 @@
 
 function comicpress_pagination() {
 	global $post, $wp_query;
-	if(function_exists('wp_pagenavi')) { ?>
-		<div id="wp-paginav">
+	if (is_paged()) {
+		if(function_exists('wp_pagenavi')) { ?>
+			<div id="wp-paginav">
 			<?php wp_pagenavi(); ?>
 			<div class="clear"></div>
-		</div>
+			</div>
 		<?php } else { ?>
-		<div id="pagenav">
-			  <div class="pagenav-right"><?php previous_posts_link('Newer Entries &uarr;') ?></div>
-			  <div class="pagenav-left"><?php next_posts_link('&darr; Previous Entries') ?></div>
+			<div id="pagenav">
+			<div class="pagenav-right"><?php previous_posts_link('Newer Entries &uarr;') ?></div>
+			<div class="pagenav-left"><?php next_posts_link('&darr; Previous Entries') ?></div>
 			<div class="clear"></div>
-		</div>
-	<?php }
+			</div>
+		<?php }
+	}
 } ?>
