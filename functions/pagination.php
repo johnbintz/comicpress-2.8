ef9397b04a9b3c0d041fedfefdda6594acebb129
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-08-28 03:33:01 -0700

    pagination fix *again*
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- functions/pagination.php
+++ functions/pagination.php
@@ -2,12 +2,8 @@
 
 function comicpress_pagination() {
 	global $post, $wp_query;
-	$paged = intval(get_query_var('paged'));
-	if(function_exists('wp_pagenavi') && !empty($paged)) { ?>
-		<div id="wp-paginav">
-		<?php wp_pagenavi(); ?>
-		<div class="clear"></div>
-		</div>
+	if(function_exists('wp_pagenavi')) { ?>
+		<?php wp_pagenavi('<div id="wp-paginav">', '<div class="clear"></div></div>'); ?>
 	<?php } else { ?>
 		<div id="pagenav">
 		<div class="pagenav-right"><?php previous_posts_link('Newer Entries &uarr;') ?></div>
