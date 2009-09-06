9059f4a82cce456e7503f7990315495ca2564d07
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-09-06 11:37:27 -0700

    signed!
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- links.php
+++ links.php
@@ -21,9 +21,11 @@ Template Name: Links
 		<?php } ?>
 
 	<?php if (is_cp_theme_style('3c,standard')) { ?>
-	<div id="content-wrapper">
+		<div id="content-wrapper">
 	<?php } ?>
+	
 	<?php get_sidebar('overblog'); ?>
+	
 	<?php if (is_cp_theme_style('3c')) get_sidebar('left'); ?>
 
 	<?php if (is_cp_theme_style('gn,standard,3c')) { ?>
@@ -50,10 +52,10 @@ Template Name: Links
 		<div class="post-page-foot"></div>
 		
 	<?php endwhile; ?>
-
+			<div class="clear"></div>
 		</div>
 	</div>
-
+	
 <?php 
 if (is_cp_theme_style('3c,v3c,gn,standard,v')) { 
 	get_sidebar('right'); ?>
@@ -64,20 +66,8 @@ if (is_cp_theme_style('3c,v3c,gn,standard,v')) {
 	<?php } ?>	
 
 	<div class="clear"></div>
-	</div>
-</div> <!-- end pageright-wrapper / content-wrapper -->
-
-<?php get_footer() ?><?php 
-if (is_cp_theme_style('3c,v3c,gn,standard,v')) { 
-	get_sidebar('right'); ?>
-<?php } ?>
-
-	<?php if (is_cp_theme_style('gn')) { ?>
-		</div>
-	<?php } ?>	
-
-	<div class="clear"></div>
 
 </div> <!-- end pageright-wrapper / content-wrapper -->
 
+<?php get_footer() ?>
 <?php get_footer() ?>
