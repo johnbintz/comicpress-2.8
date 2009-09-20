ccabf178ee63ad565d83444b36bcab80e53ef84c
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-09-19 20:27:16 -0700

    hrm wierdness
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- themepack/twopointfive/commentlink.php
+++ themepack/twopointfive/commentlink.php
@@ -1,1 +1,3 @@
-<?php if ($post->comment_status != 'closed') { ?><div class="comment-link">[ <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?> ]</div><?php } ?>
+<?php global $post; if ($post->comment_status != 'closed') { ?>
+	<div class="comment-link">[ <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?> ]</div><?php } ?>
+}
