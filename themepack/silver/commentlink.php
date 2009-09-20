60ba6097f32e6cd59e1c8f476334c3595e714281
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-09-19 20:37:50 -0700

    2.8.1.11 Commit.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- themepack/silver/commentlink.php
+++ themepack/silver/commentlink.php
@@ -1,4 +1,4 @@
-<?php if ($post->comment_status != 'closed') { ?>
+<?php global $post; if ($post->comment_status != 'closed') { ?>
 	<div class="comment-link">
 		<?php comments_popup_link('<span class="comment-balloon comment-balloon-empty">&rdquo;</span>Comment ', '<span class="comment-balloon">1</span>Comment ', '<span class="comment-balloon">%</span>Comment '); ?>
 	</div>
