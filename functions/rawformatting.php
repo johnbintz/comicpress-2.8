c35f0c5a737e712e9a471f5daf3f682564f5f561
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-10-04 23:08:56 -0700

    safd
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

---
+++ functions/rawformatting.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Raw Format Shortcode
+ * depreciated
+ */
+?>
---
+++ themepack/moosemountain/menubar.php
@@ -0,0 +1,7 @@
+<div id="moosemenubar">
+	<ul>
+		<li><a href="/feed/" class="menu-rss">RSS</a></li>
+		<li><a href="http://www.uptocampink.com " class="menu-store">Store</a></li>
+		<li><a href="mailto:info@uptocampink.com " class="menu-contact">Contact</a></li>
+	</ul>
+</div>			
