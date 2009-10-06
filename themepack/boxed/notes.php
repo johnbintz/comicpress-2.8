dcea704ae9f0a9d2bb8e0d9ad287b8f4ff1d2758
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-10-05 18:19:11 -0700

    asfd
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

---
+++ themepack/boxed/notes.php
@@ -0,0 +1,7 @@
+<strong>ThemePack</strong>: boxed<br />
+<strong>Author</strong>: ComicPress Development Team<br />
+<strong>Version</strong>: 1.4<br />
+<br />
+<strong>Installation Notes</strong>:<br />
+None<br />
+<br />
