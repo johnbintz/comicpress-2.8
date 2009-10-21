7557e13d3b078cafb24508fe254396130c947ef6
John Bintz <john@coswellproductions.com>
2009-10-20 20:48:01 -0400

    starting cleanup work for new navigation

---
+++ test/widgets/GraphicalNavigationTest.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once('MockPress/mockpress.php');
+require_once('PHPUnit/Framework.php');
+require_once(dirname(__FILE__) . '/../../widgets/graphical-navigation.php');
+
+class GraphicsNavigationTest extends PHPUnit_Framework_TestCase {
+
+}
+
+?>
