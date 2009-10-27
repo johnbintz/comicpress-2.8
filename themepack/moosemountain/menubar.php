34725c2e163a2cf280212291f9b0183480e4e5ed
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-10-27 04:11:12 -0700

    Removing MooseMountain themepack and cleaning up some stuff
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- themepack/moosemountain/menubar.php
+++
@@ -1,8 +0,0 @@
-<a href="/" id="logo">Moose Mountain</a>
-<div id="moosemenubar">
-	<ul>
-		<li><a href="/feed/" id="menu-rss">RSS</a></li>
-		<li><a href="http://www.uptocampink.com " id="menu-shop">Shop</a></li>
-		<li><a href="mailto:info@uptocampink.com " id="menu-contact">Contact</a></li>
-	</ul>
-</div>
