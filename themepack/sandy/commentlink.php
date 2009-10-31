9dbbd70510b6565174d10cf6f894ae29385f052b
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-10-30 21:41:15 -0700

    Finishing adding sandy
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

---
+++ themepack/sandy/commentlink.php
@@ -0,0 +1,3 @@
+<?php global $post; if ($post->comment_status != 'closed') { ?>
+	<div class="comment-link">[ <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?> ]</div>
+<?php } ?>
