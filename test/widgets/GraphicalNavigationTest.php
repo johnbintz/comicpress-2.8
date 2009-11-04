ad70f6da8e468360659e3509ee5e2f972cc1670a
John Bintz <john@coswellproductions.com>
2009-11-04 07:24:28 -0500

    switch back to global mockpress

--- test/widgets/GraphicalNavigationTest.php
+++ test/widgets/GraphicalNavigationTest.php
@@ -1,7 +1,6 @@
 <?php
 
-//require_once('MockPress/mockpress.php');
-require_once(dirname(__FILE__) . '/../../../mockpress/mockpress.php');
+require_once('MockPress/mockpress.php');
 require_once('PHPUnit/Framework.php');
 require_once(dirname(__FILE__) . '/../../widgets/graphical-navigation.php');
 
