1a56733824d80ee634aea7f934f671e458e9ff94
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-11-13 03:25:42 -0800

    Removing from Github for security reasons.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- sidebar-over.php
+++
@@ -1,5 +0,0 @@
-<?php if (comicpress_is_active_sidebar('Over Comic')) { ?>
-<div id="sidebar-overcomic" class="customsidebar <?php global $enable_if_widgetarea_use_sidebar_css; if ($enable_widgetarea_use_sidebar_css == 'yes') { ?> sidebar<?php } ?>">
-	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Over Comic') ) : ?><?php endif; ?>
-</div>
-<?php } ?>
