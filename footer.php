7f3381016e334016351d2b14e1f5ca96c0975e94
mindfaucet <tyler@mindfaucet.com>
2009-11-17 16:07:17 -0700

    Tightened up footer to avoid line breaks on narrower layout. Set copyright name to default to the blog title if the admin user does not have a name entered.

--- footer.php
+++ footer.php
@@ -9,10 +9,31 @@
 	<?php get_sidebar('footer'); ?>
 	<?php global $disable_footer_text, $wpmu_version; if ($disable_footer_text != 'yes') { ?>
 		<p>
-			<span class="footer-copyright"><?php echo cp_copyright(); ?> <?php if (empty($wpmu_version)) { ?><?php echo the_author_meta('firstname',1); echo "&nbsp;"; echo the_author_meta('lastname',1); ?><span class="footer-pipe"> | </span><?php } ?></span>
-			<span class="footer-powered"><?php bloginfo('name'); ?> <?Php _e('Powered by','comicpress'); ?> <a href="http://wordpress.org/">WordPress</a> <?php _e('with','comicpress'); ?> <a href="http://comicpress.org/">ComicPress</a><span class="footer-pipe"> | </span></span>
-			<span class="footer-subscribe">Subscribe: <a href="<?php bloginfo('rss2_url') ?>">RSS Feed</a><span class="footer-pipe"> | </span></span>
-			<span class="footer-uptotop"><a href="#outside" onclick="scrollup(); return false;"><?php _e('Back to Top &uarr;','comicpress'); ?></a></span>
+			<span class="footer-copyright">
+				<?php echo cp_copyright(); ?>
+				<?php if (empty($wpmu_version)) {
+					$authorfirstname = get_the_author_meta('firstname',1);
+					$authorlastname = get_the_author_meta('lastname',1);
+					$blogname = get_bloginfo('title');
+					if ( $authorfirstname != "" ) {
+						echo $authorfirstname."	".$authorlastname;
+					} else {
+						echo $blogname;
+					}
+				} ?>
+			</span>
+			<span class="footer-powered">
+				<span class="footer-pipe">|</span>
+				<?php _e('Powered by','comicpress'); ?> <a href="http://wordpress.org/">WordPress</a> <?php _e('with','comicpress'); ?> <a href="http://comicpress.org/">ComicPress</a>
+			</span>
+			<span class="footer-subscribe">
+				<span class="footer-pipe">|</span>
+				Subscribe: <a href="<?php bloginfo('rss2_url') ?>">RSS</a>
+			</span>
+			<span class="footer-uptotop">
+				<span class="footer-pipe">|</span>
+				<a href="#outside" onclick="scrollup(); return false;"><?php _e('Back to Top &uarr;','comicpress'); ?></a>
+			</span>
 			<!-- <?php echo get_num_queries() ?> queries. <?php timer_stop(1) ?> seconds. -->
 		</p>
 	<?php } ?>
