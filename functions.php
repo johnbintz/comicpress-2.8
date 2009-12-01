aa9eaa02929b2349abea9139db03680e5f320d4f
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-12-01 13:57:06 -0800

    2.9.0.4 tagged ?
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- functions.php
+++ functions.php
@@ -75,9 +75,7 @@ function comicpress_load_options() {
 	global $comicpress_options;
 	$comicpress_options = get_option('comicpress_options');
 	if (empty($comicpress_options)) {
-<<<<<<< .mine		$comicpress_options['comicpress_version'] = '2.9.0.4';
-=======		$comicpress_options['comicpress_version'] = '2.9.0.4'; 
->>>>>>> .theirs
+		$comicpress_options['comicpress_version'] = '2.9.0.4'; 
 		foreach (array(
 			'disable_comic_frontpage' => false,
 			'disable_comic_blog_frontpage' => false,
