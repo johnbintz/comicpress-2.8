1f428c786a8bca4e4d2f11b670871166ca6785b8
John Bintz <john@coswellproductions.com>
2009-12-01 21:37:28 -0500

    filtering

--- test/ComicPressMediaHandlingTest.php
+++ test/ComicPressMediaHandlingTest.php
@@ -9,6 +9,10 @@ class ComicPressMediaHandlingTest extends PHPUnit_Framework_TestCase {
 	function setUp() {
 		_reset_wp();
 		$this->cpmh = new ComicPressMediaHandling();
+		$this->default_filter = $this->cpmh->default_filter;
+
+		vfsStreamWrapper::register();
+		vfsStreamWrapper::setRoot(new vfsStreamDirectory('root'));
 	}
 
 	function testBundleGlobalVariables() {
@@ -28,10 +32,12 @@ class ComicPressMediaHandlingTest extends PHPUnit_Framework_TestCase {
 	}
 
 	function providerTestGetFilter() {
+		$cpmh = new ComicPressMediaHandling();
+
 		return array(
-			array(null, '%wordpress%/%type-folder%/%date-Y-m-d%*.*'),
-			array('fail', '%wordpress%/%type-folder%/%date-Y-m-d%*.*'),
-			array(array(), '%wordpress%/%type-folder%/%date-Y-m-d%*.*'),
+			array(null, $cpmh->default_filter),
+			array('fail', $cpmh->default_filter),
+			array(array(), $cpmh->default_filter),
 			array('test', 'test')
 		);
 	}
@@ -44,6 +50,51 @@ class ComicPressMediaHandlingTest extends PHPUnit_Framework_TestCase {
 
 		$comic_filename_filters['test'] = 'test';
 
-		$this->assertEquals($expected_result, $this->cpmh->_get_filter($filter_to_use));
+		$cpmh = $this->getMock('ComicPressMediaHandling', array('_convert_to_percent_filter'));
+		if ($expected_result !== $cpmh->default_filter) {
+			$cpmh->expects($this->once())->method('_convert_to_percent_filter')->with($expected_result)->will($this->returnValue($expected_result));
+		} else {
+			$cpmh->expects($this->never())->method('_convert_to_percent_filter');
+		}
+
+		$this->assertEquals($expected_result, $cpmh->_get_filter($filter_to_use));
+	}
+
+	function providerTestConvertToPercentFilter() {
+		return array(
+			array('', '%wordpress%/%type-folder%/'),
+			array('{date}', '%wordpress%/%type-folder%/%date-Y-m-d%'),
+			array('%wordpress%/%type-folder%/{date}', '%wordpress%/%type-folder%/{date}'),
+		);
+	}
+
+	/**
+	 * @dataProvider providerTestConvertToPercentFilter
+	 */
+	function testConvertToPercentFilter($old_filter, $new_filter) {
+		$this->assertEquals($new_filter, $this->cpmh->_convert_to_percent_filter($old_filter));
+	}
+
+	function providerTestExpandFilter() {
+		return array(
+			array('', ''),
+			array('%test%', '%test%'),
+			array('%wordpress%', vfsStream::url('root')),
+			array('%wordpress%%wordpress%', vfsStream::url('root') . vfsStream::url('root')),
+			array('%test test%', '%test test%'),
+			array('%wordpress%/%type-folder%', vfsStream::url('root') . '/comic'),
+			array('%date-Y%', '2009'),
+			array('%wordpress%/%type-folder%/%date-Y-m-d%*.*', vfsStream::url('root') . '/comic/2009-01-01.*\..*'),
+		);
+	}
+
+	/**
+	 * @dataProvider providerTestExpandFilter
+	 */
+	function testExpandFilter($filter, $expected_result) {
+		$cpmh = $this->getMock('ComicPressMediaHandling', array('_abspath'));
+		$cpmh->expects($this->any())->method('_abspath')->will($this->returnValue(vfsStream::url('root')));
+
+		$this->assertEquals($expected_result, $cpmh->_expand_filter($filter, 'comic', (object)array('ID' => 1, 'post_date' => '2009-01-01 15:00:00')));
 	}
 }
