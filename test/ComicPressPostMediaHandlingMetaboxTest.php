5a717b627fed53eec050a71d294c6f09f3bef4ad
John Bintz <john@coswellproductions.com>
2009-12-04 22:36:00 -0500

    handle post media update

--- test/ComicPressPostMediaHandlingMetaboxTest.php
+++ test/ComicPressPostMediaHandlingMetaboxTest.php
@@ -5,16 +5,31 @@ require_once('MockPress/mockpress.php');
 require_once(dirname(__FILE__) . '/../classes/ComicPressPostMediaHandlingMetabox.inc');
 
 class ComicPressPostMediaHandlingMetaboxTest extends PHPUnit_Framework_TestCase {
-	function providerTestUpdate() {
+	function setUp() {
+		_reset_wp();
+		$this->pmh = new ComicPressPostMediaHandlingMetabox();
+	}
+
+	function providerTestPostMediaUpdate() {
 		return array(
-			array(array(), array())
+			array(array(), ''),
+			array(array('post_id' => 'test'), ''),
+			array(array('post_id' => 1), array()),
+			array(array('post_id' => 1, 'urls' => false), array()),
+			array(array('post_id' => 1, 'urls' => array()), array()),
+			array(array('post_id' => 1, 'urls' => array('test' => 'test')), array()),
+			array(array('post_id' => 1, 'urls' => array('comic' => 'test')), array('comic' => 'test')),
 		);
 	}
 
 	/**
-	 * @dataProvider providerTestUpdate
+	 * @dataProvider providerTestPostMediaUpdate
 	 */
-	function testUpdate($input, $expected_post_metadata) {
+	function testPostMediaUpdate($input, $expected_post_metadata) {
+		$pmh = $this->getMock('ComicPressPostMediaHandlingMetabox', array('_get_valid_types'));
+		$pmh->expects($this->any())->method('_get_valid_types')->will($this->returnValue(array('comic')));
 
+		$this->pmh->handle_post_media_update($input);
+		$this->assertEquals($expected_post_metadata, get_post_meta(1, 'backend_url_images', true));
 	}
 }
