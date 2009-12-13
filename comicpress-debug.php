f2e33119d3410c30320069c2814127051a2c86fb
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-12-13 10:16:19 -0800

    Additional comicpress-debug.php install checking and the v3cr.png file for the new layout.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- comicpress-debug.php
+++ comicpress-debug.php
@@ -16,13 +16,19 @@ function comicpress_notice_debug() {
 		$error[] = array('header', __('Primary Comic and Blog categories are not configured properly.','comicpress'));
 		$error[] = __('ComicPress requires 2 categories to be added to the ','comicpress') . '<a href="'.$post_cat_link.'">' . __('post categories.','comicpress') . '</a>' . 
 			__(' It is necessary to have 2 more categories in addition to the uncategorized category, a Blog and Comic primary categories.  These two additional categories will be the root categories that seperate the difference between the comic and blog posts.  When you post a new comic you will be posting it into the comic category or heirarchal children of the comic category.   When posting a new blog post you need to set it into the blog category or child of the blog category.   Uncategorized will act as a blog post category (do not rename uncategorized).  You can configure the categories to set as the primary blog and comic category from within the comicpress-config.php file or use ComicPress Manager - ComicPress Config','comicpress');
-	}	
+	}
 	
 	// Check Comics Folder
 	if (!is_dir(ABSPATH . '/' . $comic_folder)) {
 		$error[] = array('header', __('Comics Folder is not configured and is unable to be found.','comicpress'));
 		$error[] = __('ComicPress stores the files it uses inside a specific directory and that directory is set within the comicpress-config.php or you can configure it from within ComicPress Manager.  When this error is present it means that the theme is unable to find the appropriate directory to read the comics from.','comicpress');
-	}		
+	}
+	
+	// Make sure the ComicPress theme is installed in themes/comicpress
+	if (ABSPATH . 'wp-content/themes/comicpress' != get_template_directory()) {
+		$error[] = array('header', __('ComicPress theme is not installed into the correct folder.','comicpress'));
+		$error[] = __('As of version 2.9, the ComicPress main core theme is required to be installed into the wp-content/themes/comicpress directory.  It is currently not installed into that directory.','comicpress');
+	}
 	
 	if (!empty($error)) {
 	?>
