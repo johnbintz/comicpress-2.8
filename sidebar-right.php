1ceb695ffc3960a7b640390de53fc19f24d343fa
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-12-21 14:26:24 -0800

    MenubarWidget.inc LatestComicsWidget.inc dep'd latestcomics.php, revamped sidebar-left.php and sidebar-right.php to use the_widget()
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- sidebar-right.php
+++ sidebar-right.php
@@ -3,25 +3,18 @@ if (!$comicpress_options['disable_lrsidebars_frontpage']) { ?>
 <div id="sidebar-right">
 	<div class="sidebar-head"></div>
 		<div class="sidebar">
-			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : ?>
-				<?php the_widget('BookmarkWidget',array(),array()); ?>
-				<?php if (is_cp_theme_layout('standard,v')) { ?>
-					<div class="widget">
-						<?php comicpress_calendar_embed(); ?>
-					</div>
-				<?php } ?>
-				<div class="widget">
-				<ul><li>
-					<h2>Menu</h2>
-						<ul>
-							<?php wp_list_pages('title_li=') ?>
-						</ul>
-				</li></ul>
-				</div>
-				<div class="widget">
-					<ul><?php wp_list_categories('title_li=<h2>Categories</h2>') ?></ul>
-				</div>
-			<?php endif; ?>
+		<?php 
+		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : 
+			the_widget('BookmarkWidget','mode=three-button');
+			if (is_cp_theme_layout('standard,v')) { 
+				the_widget('CalendarWidget');
+				the_widget('ArchiveDropdownWidget','mode=monthly_archive'); 
+			}
+			the_widget('WP_Widget_Pages');
+			the_widget('WP_Widget_Categories','hierarchical=1&count=1');
+
+		endif; 
+		?>
 		</div>
 	<div class="sidebar-foot"></div>
 </div>
