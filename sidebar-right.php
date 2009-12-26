800a840be795d83330f0bd068bc52d33ca344e6b
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-12-26 06:43:43 -0800

    removed bookmarkwidget from being a default widget because of the lack of ability to init the scripts for it.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- sidebar-right.php
+++ sidebar-right.php
@@ -5,7 +5,6 @@ if (!$comicpress_options['disable_lrsidebars_frontpage']) { ?>
 		<div class="sidebar">
 		<?php 
 		if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Right Sidebar') ) : 
-			the_widget('ComicpressBookmarkWidget','mode=three-button');
 			if (is_cp_theme_layout('standard,v')) { 
 				the_widget('ComicPressCalendarWidget');
 				the_widget('ComicPressArchiveDropdownWidget', 'mode=storyline_order');
@@ -15,7 +14,6 @@ if (!$comicpress_options['disable_lrsidebars_frontpage']) { ?>
 			}
 			the_widget('WP_Widget_Pages');
 			the_widget('WP_Widget_Categories','hierarchical=1&count=1');
-
 		endif; 
 		?>
 		</div>
