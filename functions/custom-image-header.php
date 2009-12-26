7ab9c22c9c0a8b6782536f427ec5d2235818dfc2
Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>
2009-12-26 05:54:22 -0800

    CSS adjustments for the custom-image-header.php apparently there was an extra } all this time.
    Signed-off-by: Philip M. Hofer (Frumph) <frumph_dragon@yahoo.com>

--- functions/custom-image-header.php
+++ functions/custom-image-header.php
@@ -36,34 +36,34 @@ if ($comicpress_options['enable_custom_image_header']) {
 	
 	function theme_header_style() {
 		?>
-		<style type="text/css">
-		#header
-		{
-		width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
-		height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
-		background: url(<?php header_image(); ?>) no-repeat center;
-		}
-		
-		<?php
+
+<style type="text/css">
+#header {
+	width: <?php echo HEADER_IMAGE_WIDTH; ?>px;
+	height: <?php echo HEADER_IMAGE_HEIGHT; ?>px;
+	background: url(<?php header_image(); ?>) center center no-repeat;
+}
+
+<?php
 		//  Has the text been hidden?
 		//  If so, set display to equal none
 		if ( 'blank' == get_header_textcolor() ) { ?>
-			#header h1, #header .description {
-			display: none;
-			}	
-		<?php } else {
-			//  Otherwise, set the color to be the user selected one
-			?>
-			#header *
-			{
-			color: #<?php header_textcolor();?>;
-			}
-			}
-		<?php } ?>
-		</style>
-		<?php
-	}
-	
+#header h1, #header .description {
+	display: none;
+}
+
+<?php } else {
+//  Otherwise, set the color to be the user selected one
+?>
+
+#header * {
+	color: #<?php header_textcolor();?>;
+}
+<?php } ?>
+</style>
+
+<?php }
+
 	if ( function_exists('add_custom_image_header') ) {
 		add_custom_image_header('theme_header_style', 'theme_admin_header_style');
 	}
