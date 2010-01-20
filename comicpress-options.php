001f51fe6519ac76acc2d13c05bfbdc2a0a1776a
John Bintz <john@coswellproductions.com>
2010-01-20 07:35:17 -0500

    add new filter filtering option and associated test

--- comicpress-options.php
+++ comicpress-options.php
@@ -19,6 +19,23 @@ function comicpress_admin_print_styles() {
 	wp_enqueue_style('comicpress-options', get_template_directory_uri() . '/options/options.css');
 }
 
+function comicpress_save_options_comic_filename_filters($incoming) {
+	$filters = array();
+
+	foreach (array_values($incoming) as $filter) {
+		$filters[wp_filter_nohtml_kses($filter['name'])] = wp_filter_nohtml_kses($filter['filter']);
+	}
+
+	if (!empty($filters)) {
+		if (!isset($filters['default'])) {
+			$cpmh = new ComicPressMediaHandling();
+			$filters['default'] = $cpmh->default_filter;
+		}
+	}
+
+	return $filters;
+}
+
 function comicpress_admin_page_head() { ?>
 	<!--[if lt ie 8]> <style> div.show { position: static; margin-top: 1px; } #cpadmin div.off { height: 22px; } </style> <![endif]-->
 <?php }
